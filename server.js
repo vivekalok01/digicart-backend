01295c0e22456954055f69945520496a7de081a0
vivekalok01 <vivekalok321@gmail.com>
2025-05-22 20:56:54 +0530

    addd

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // CORS allowed frontend origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = process.env.FRONTEND_URL;
 
 app.use(cors({
   origin: function (origin, callback) {
